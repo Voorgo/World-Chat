678ba1c442becd746c8496eaeeffd349b72bf9f1
Voorgo <serbancatalin10@yahoo.com>
2023-05-20 10:22:13 +0300

    reafactor: Remove favicon

--- client/src/assets/topics.js
+++ client/src/assets/topics.js
@@ -1,36 +1,138 @@
 export const topics = [
-  "Actors/ Actresses",
-  "Aliens",
-  "Anime",
-  "Art",
-  "Books",
-  "Cars",
-  "Children",
-  "Clothes",
-  "Clubs",
-  "Comedy",
-  "Cooking",
-  "Current trends",
-  "Fishing",
-  "Food",
-  "Games",
-  "Heroes",
-  "Hiking",
-  "Hobbies",
-  "Horror",
-  "Job",
-  "Languages",
-  "Learning",
-  "Movies",
-  "Music",
-  "News",
-  "Pets",
-  "Photography",
-  "Podcasts",
-  "Programming",
-  "Restaurants",
-  "Shopping",
-  "Social Media",
-  "Sports",
-  "Travel",
-].sort();
+  {
+    label: "Actors or Actresses",
+    value: "Actors or Actresses",
+  },
+  {
+    label: "Aliens",
+    value: "Aliens",
+  },
+  {
+    label: "Anime",
+    value: "Anime",
+  },
+  {
+    label: "Art",
+    value: "Art",
+  },
+  {
+    label: "Books",
+    value: "Books",
+  },
+  {
+    label: "Cars",
+    value: "Cars",
+  },
+  {
+    label: "Children",
+    value: "Children",
+  },
+  {
+    label: "Clothes",
+    value: "Clothes",
+  },
+  {
+    label: "Clubs",
+    value: "Clubs",
+  },
+  {
+    label: "Comedy",
+    value: "Comedy",
+  },
+  {
+    label: "Cooking",
+    value: "Cooking",
+  },
+  {
+    label: "Current trends",
+    value: "Current trends",
+  },
+  {
+    label: "Fishing",
+    value: "Fishing",
+  },
+  {
+    label: "Food",
+    value: "Food",
+  },
+  {
+    label: "Games",
+    value: "Games",
+  },
+  {
+    label: "Heroes",
+    value: "Heroes",
+  },
+  {
+    label: "Hiking",
+    value: "Hiking",
+  },
+  {
+    label: "Hobbies",
+    value: "Hobbies",
+  },
+  {
+    label: "Horror",
+    value: "Horror",
+  },
+  {
+    label: "Job",
+    value: "Job",
+  },
+  {
+    label: "Languages",
+    value: "Languages",
+  },
+  {
+    label: "Learning",
+    value: "Learning",
+  },
+  {
+    label: "Movies",
+    value: "Movies",
+  },
+  {
+    label: "Music",
+    value: "Music",
+  },
+  {
+    label: "News",
+    value: "News",
+  },
+  {
+    label: "Pets",
+    value: "Pets",
+  },
+  {
+    label: "Photography",
+    value: "Photography",
+  },
+  {
+    label: "Podcasts",
+    value: "Podcasts",
+  },
+  {
+    label: "Programming",
+    value: "Programming",
+  },
+  {
+    label: "Restaurants",
+    value: "Restaurants",
+  },
+  {
+    label: "Shopping",
+    value: "Shopping",
+  },
+  {
+    label: "Social Media",
+    value: "Social Media",
+  },
+  {
+    label: "Sports",
+    value: "Sports",
+  },
+  {
+    label: "Travel",
+    value: "Travel",
+  },
+];
