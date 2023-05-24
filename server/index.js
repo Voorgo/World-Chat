9460b783d571986819c69811511895cd5d083564
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-05-24 16:05:52 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ const httpServer = createServer(app);
 const PORT = process.env.PORT || 3030;
 const socketIO = new Server(httpServer, {
   cors: {
-    origin: "*",
+    origins: '*:*',
+      transports: ['polling'], 
     credentials: true,
      methods: ["GET", "POST"]
   }
