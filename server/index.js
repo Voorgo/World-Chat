6a6b7b893ac8199e8b9718742161c82b5cb9edd5
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-05-24 12:17:34 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ const socketIO = new Server(httpServer, {
 });
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true}));
