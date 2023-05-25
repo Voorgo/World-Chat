b44f91666f2b9ada076a990e5dfd57d1c0539eec
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-05-25 10:12:11 +0300

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { Register, Login, Logout } from "../controllers/users.js";
+import { Register, Login, Logout, Admins } from "../controllers/users.js";
 import { refreshToken } from "../controllers/refreshToken.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.post("/users", Register);
 router.post("/login", Login);
 router.get("/token", refreshToken);
 router.delete("/logout", Logout);
+router.get("/admins", Admins);
 
 export default router;
