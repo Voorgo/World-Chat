b4ba47e02961b0191311f1af08901111653d87c6
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-05-25 12:06:03 +0300

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,7 +8,13 @@ import {
   Route,
 } from "react-router-dom";
 import { Root, Home, Register, Login, Chat } from "./pages";
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['xsrfCookieName'] = 'CSRFToken';
+axios.defaults.headers.post['xsrfHeaderName'] = 'X-CSRFToken';
+axios.defaults.headers.post['responseType'] = 'json';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
