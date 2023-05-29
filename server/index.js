b5ce75e060deaef18aa1cd48d29f313dcc7269a2
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-05-29 17:21:09 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const httpServer = createServer(app);
 const PORT = process.env.PORT || 5000;
 const socketIO = new Server(httpServer, {
  cors: {
-    origin: "http://localhost:5173/",
+    origin: "http://localhost:5173",
    credentials:true, 
   }
 });
