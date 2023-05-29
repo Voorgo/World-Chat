ed38da38da654d8883ac85bb03c45543520a26ff
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-05-29 17:15:54 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,20 +16,10 @@ const PORT = process.env.PORT || 5000;
 const socketIO = new Server(httpServer, {
  cors: {
     origin: "http://localhost:5173/",
+   credentials:true, 
   }
 });
-app.use(function(req, res, next) {
-      // res.header("Access-Control-Allow-Origin", "*");
-      const allowedOrigins = ['http://localhost:5173'];
-      const origin = req.headers.origin;
-      if (allowedOrigins.includes(origin)) {
-           res.setHeader('Access-Control-Allow-Origin', origin);
-      }
-      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-      res.header("Access-Control-Allow-credentials", true);
-      res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, UPDATE");
-      next();
-    });
+
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
