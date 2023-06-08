1fe76cca76269d1fd361c3f2e5cba1319fc0b8a8
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-06-08 11:16:42 +0300

    Update ChatInput.jsx

--- client/src/components/ChatInput.jsx
+++ client/src/components/ChatInput.jsx
@@ -9,7 +9,6 @@ const ChatInput = () => {
   const location = useLocation();
 
   const sendMessage = (e) => {
-    console.log(location.state.room)
     e.preventDefault();
     if (message !== "") {
       const createdtime = Date.now();
