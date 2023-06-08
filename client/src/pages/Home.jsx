f71ef75a6818a3172c43c5f8f3c127ed4ff905cf
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-06-08 12:39:04 +0300

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   }, [socket]);
 
   return (
-    <div className="h-full w-full bg-gradient-to-b from-[#272D41] via-[#000519] to-[#0e1516]">
+    <div className="h-[100vh] w-full bg-gradient-to-b from-[#272D41] via-[#000519] to-[#0e1516]">
       <div className="relative flex h-full w-full flex-col-reverse sm:flex-row">
         <Sidebar />
         <Scene />
