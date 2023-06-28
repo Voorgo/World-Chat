a150968c22cecf3004a6adb9309e05bc12a96579
Voorg <84379991+Voorgo@users.noreply.github.com>
2023-06-28 14:09:18 +0300

    Update AccordionItem.jsx

--- client/src/components/AccordionItem.jsx
+++ client/src/components/AccordionItem.jsx
@@ -8,13 +8,22 @@ const AccordionItem = ({ header, ...rest }) => {
       header={({ state: { isEnter } }) => (
         <>
           {header}
-          <img
-            className={`ml-auto transition-transform duration-200 ease-in-out ${
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className={`ml-auto transition-transform duration-200 ease-in-out h-6 w-6 ${
               isEnter && "rotate-180"
             }`}
-            src={`/src/assets/arrow.svg`}
-            alt="arrow"
-          />
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
         </>
       )}
       className="border-b"
